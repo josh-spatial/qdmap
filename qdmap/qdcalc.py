33882f61164ec6999b71874e8b4fa65cb605a1bf
josh-spatial <72358384+josh-spatial@users.noreply.github.com>
2023-03-25 10:10:28 -0400

    Create qdcalc module, tweak PES class

---
+++ qdmap/qdcalc.py
@@ -0,0 +1,14 @@
+"""This module contains the basic QD calculation functions."""
+
+# Define blast distance (BD) functions.
+def bd1(q):
+    """Returns BD1 blast distance from a given
+      quantity (Q) in kg."""
+    # import numpy as np
+    # bd1 = np.where(q < 30_000, 0.35 * q ** (1/3), 0.44 * q ** (1/3))
+    # return bd1
+    if q < 30_000:
+        bd1 = 0.35 * q ** (1/3) 
+    else:
+        bd1 = 0.44 * q ** (1/3)
+    return bd1
--- qdmap/site.py
+++ qdmap/site.py
@@ -1,8 +1,11 @@
 # This module defines the classes for sites used in qdmap.
 #=========================
-# TODO fix the errors in the bd1() function. Maybe I should rethink how I'm doing this.
+# DONE fix the errors in the bd1() function. Maybe I should rethink how I'm doing this.
 # Maybe I can make a child class of Site for PESite. In that class is where I bring in the Limit class
 # or just leave it as attributes within the PESite class? I think I'm making this too complicated.
+
+import qdcalc as qd
+
 
 
 class Site:
@@ -23,25 +26,28 @@ class PES(Site):
         super().__init__(facility_no, type)
         self.neq_1_1 = 0
         self.neq_1_2_1 = 0
+        self.mce_1_2_1 = 0
         self.neq_1_2_2 = 0
         self.neq_1_2_3 = 0
+        self.bd1_1_1 = qd.bd1(self.neq_1_1)
+        self.bd1_2_1 = qd.bd1(self.mce_1_2_1)
 
     def limit_info(self):
         """Return formatted info about the NEQ limits."""
         limit_info = f"\tHD 1.1: \t{self.neq_1_1} kg \n\tHD 1.2.1: \t{self.neq_1_2_1} kg"
         return limit_info
 
-    def bd1_1_1(self):
-        """Returns BD1 blast distance from a given
-          quantity (Q) in kg."""
-        # import numpy as np
-        # bd1 = np.where(q < 30_000, 0.35 * q ** (1/3), 0.44 * q ** (1/3))
-        # return bd1
-        if self.neq_1_1 < 30_000:
-            self.bd1_1_1 = 0.35 * self.neq_1_1 ** (1/3) 
-        else:
-            self.bd1_1_1 = 0.44 * self.neq_1_1 ** (1/3)
-        return self.bd1_1_1
+    # def bd1_1_1(self):
+    #     """Returns BD1 blast distance from a given
+    #       quantity (Q) in kg."""
+    #     # import numpy as np
+    #     # bd1 = np.where(q < 30_000, 0.35 * q ** (1/3), 0.44 * q ** (1/3))
+    #     # return bd1
+    #     if self.neq_1_1 < 30_000:
+    #         self.bd1_1_1 = 0.35 * self.neq_1_1 ** (1/3) 
+    #     else:
+    #         self.bd1_1_1 = 0.44 * self.neq_1_1 ** (1/3)
+    #     return self.bd1_1_1
 
     def qd_info(self):
         """Return formatted info about the QDs."""
@@ -97,6 +103,9 @@ s1.neq_1_2_1 = input(f"Enter Facility No. {s1.facility_no}'s HD 1.2.1 limit (kg)
 s1.neq_1_2_1 = float(s1.neq_1_2_1)
 print(s1.site_info())
 print(s1.limit_info())
-print(f"\t\tBD1: {s1.bd1_1_1():.1f} m")
-# print(s1.qd_info())
+# print(f"\t\tBD1: {s1.bd1_1_1():.1f} m") # TODO Bug: Float not callable?
+
+# Try to interate over the attributes:
+for attribute, value in s1.__dict__.items():
+    print(attribute, value) # TODO Figure out why the bd1_1_1 is still zero after entering a value.
     
